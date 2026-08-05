a7d60c035acc8635ee7e3b564d3b2e4631b5de76
Rittick Datta <drittick@gmail.com>
2026-08-05 11:53:48 +0200

    feat: work on navbar and home page

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,5 +1,29 @@
-export default function Home(){
-    return (<>
-        Home
-    </>)
+import { useNavigate } from 'react-router-dom';
+import Button from '../shared/Button';
+import styles from './Home.module.css';
+
+export default function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <section className={styles.homeContainer}>
+      <div>
+        <h1 className={styles.appTitle}>
+          Welcome to the Restaurant Table Manager
+        </h1>
+        <p className={styles.appDescription}>
+          Manage your restaurant tables, menu items, and orders with ease.
+        </p>
+      </div>
+
+      <div className={styles.actions}>
+        <Button
+          text="Get Started"
+          onClick={() => {
+            navigate('/admin');
+          }}
+        />
+      </div>
+    </section>
+  );
 }
--- src/app/layout/RootLayout.tsx
+++ src/app/layout/RootLayout.tsx
@@ -1,18 +1,59 @@
 import { Link, Outlet } from 'react-router-dom';
+import styles from './RootLayout.module.css';
 
 export default function RootLayout() {
   return (
     <>
-      <header>
-        <Link to="/">Home</Link>
-        <Link to="/admin">Admin</Link>
-        <Link to="/staff">Staff</Link>
-        <Link to="/tables">Tables</Link>
-        <Link to="/menu">Menu</Link>
-        <Link to="/orders">Orders</Link>
+      <header className={styles.appHeader}>
+        <span>
+          <Link
+            to="/"
+            className={styles.appTitle}
+          >
+            Restaurant Table Manager
+          </Link>
+        </span>
+        <div className={styles.navLinks}>
+          <Link
+            className={styles.navLink}
+            to="/"
+          >
+            Home
+          </Link>
+          <Link
+            className={styles.navLink}
+            to="/admin"
+          >
+            Admin
+          </Link>
+          <Link
+            className={styles.navLink}
+            to="/staff"
+          >
+            Staff
+          </Link>
+          <Link
+            className={styles.navLink}
+            to="/tables"
+          >
+            Tables
+          </Link>
+          <Link
+            className={styles.navLink}
+            to="/menu"
+          >
+            Menu
+          </Link>
+          <Link
+            className={styles.navLink}
+            to="/orders"
+          >
+            Orders
+          </Link>
+        </div>
       </header>
 
-      <main>
+      <main className={styles.appMain}>
         <Outlet />
       </main>
     </>
